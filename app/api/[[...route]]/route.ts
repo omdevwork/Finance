e47ce503a5e46c3f86af3b2c312ef669f0c40bd1
Om Davra <dhruvidavra@Oms-MacBook-Pro.local>
2024-09-19 14:13:15 +0530

    initial commit

---
+++ app/api/[[...route]]/route.ts
@@ -0,0 +1,33 @@
+import accounts from "./accounts";
+import categories from "./categories";
+import transactions from "./transactions";
+import summary from "./summary";
+
+import { Hono } from "hono";
+import { HTTPException } from "hono/http-exception";
+import { handle } from "hono/vercel";
+
+export const runtime = "edge";
+
+const app = new Hono().basePath("/api");
+
+app.onError((err, c) => {
+    if (err instanceof HTTPException) {
+        return err.getResponse();
+    }
+
+    return c.json({ error: "Internal server error" }, 500);
+})
+
+const routes = app
+    .route("/accounts", accounts)
+    .route("/categories", categories)
+    .route("/transactions", transactions)
+    .route("/summary", summary);
+
+export const GET = handle(app);
+export const POST = handle(app);
+export const PATCH = handle(app);
+export const DELETE = handle(app);
+
+export type AppType = typeof routes;
